c7d21d190fe6eb46e3ed75bcc60f865ac828fd2b
11249A073-Nitheesh <11249a073@kanchiuniv.ac.in>
2026-04-26 11:14:16 +0530

    Update Tower_of_hanoi.c

--- DAA/Tower_of_hanoi.c
+++ DAA/Tower_of_hanoi.c
@@ -1,13 +1,25 @@
+Aim : To Write a C program to solve the Tower of Hanoi Using Recursion.
+//program//
+
 #include <stdio.h>
-          void TOH(int n, char S, char A, char D)
-          {
+
+// recursive function to perform tower of hanoi
+void TOH(int n, char S, char A, char D)
+ {
+           // base case : if only one disk
               if (n == 1)
               {
                   printf("Move disk 1 from %c to %c\n", S, D);
                   return;
               }
-              TOH(n - 1, S, D, A);
+
+              // move n-1 disks from source to temporary
+              TOH(n - 1, S, D, A);     
+
+              // move nth disk from source to destination
               printf("Move disk %d from %c to %c\n", n, S, D);
+
+              // move n-1 disks from temporary to destination
               TOH(n - 1, A, S, D);
           }
           int main()
@@ -16,6 +28,8 @@
               printf("Enter the number of disks: ");
               scanf("%d", &n);
               printf("\nRequired moves:\n");
+
+              // function call      
               TOH(n, 'S', 'A', 'D');
               return 0;
           }
